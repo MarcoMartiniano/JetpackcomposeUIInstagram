ada5ce050f8a9c1b1facb1f174a04bdeaee7f6ba
MarcoMartiniano <79107727+MarcoMartiniano@users.noreply.github.com>
2021-06-29 13:27:51 -0300

    first commit
    first commit

---
+++ app/src/main/java/com/marco/portifolioinstagramui/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.marco.portifolioinstagramui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+
+
+
+class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ProfileScreen()
+        }
+    }
+}
